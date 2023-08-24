42307ef0cce8e9a91ca9a6b751ba8d69d631fb56
FerchosCC <142555804+FerchosCC@users.noreply.github.com>
2023-08-24 10:49:03 -0600

    CuartoTrabajoArray.py

--- CuartoTrabajoArray.py
+++ CuartoTrabajoArray.py
@@ -1,21 +1,20 @@
 Factura=["pan","huevos",100,1234]
-
-print(Factura)
+print(Factura,"\n")
 
 print(Factura[0])
-print(Factura[3])
+print(Factura[3],"\n")
 
 print(Factura[-1])
 print(Factura[-2])
 print(Factura[-3])
-print(Factura[-4])
+print(Factura[-4],"\n")
 
 print(len(Factura))
 print(len(Factura)-1)
-print(Factura[-len(Factura)])
+print(Factura[-len(Factura)],"\n")
 
 Factura[1]="Carne"
-print(Factura)
+print(Factura,"\n")
 
 factura=["pan","carne",100,1234]
 electronica=["telefono",500]
